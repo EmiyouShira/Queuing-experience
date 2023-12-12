429d66df1fbd1081c0e87f7644c33b4c367b03eb
dvdvideo1234 <dvd_video@abv.bg>
2023-12-12 21:22:19 +0200

    Added: Automatic NPC added to the list Added: Force walk from position 1 to position 2 Added: Force remove invalid NPC

--- lua/autorun/hooks_npc_orig.lua
+++ lua/autorun/hooks_npc_orig.lua
@@ -1,3 +1,10 @@
+-- Move NPC to the desired position with forced walking
+local function npcMoveTo(npc, pos)
+	if not IsValid(npc) then return end
+	npc:SetSaveValue("m_vecLastPosition", pos)
+	npc:SetSchedule(SCHED_FORCED_GO)
+end
+
 -- Function to check if there's someone in front of the NPC in the line
 local function isSomeoneInFront(npc, nodPos)
     local npcPos = npc:GetPos()
@@ -15,18 +22,17 @@ local function isSomeoneInFront(npc, nodPos)
 end
 
 -- NPC Names and Entity IDs
-local npcEntities = {
-    {name = "npc_mossman", id = 257},
-    {name = "npc_breen", id = 873},
-    {name = "npc_gman", id = 874},
-    {name = "npc_eli", id = 875},
-    {name = "npc_alyx", id = 876},
-    -- Add more NPCs as needed
-}
+local npcEntities = {}
+
+hook.Add("PlayerSpawnedNPC", "hook_npc_queue",
+  function(ply, npc)
+    if not IsValid(npc) then return end
+    table.insert(npcEntities, {name = npc:GetClass(), id = npc:EntIndex()})
+  end)
 
 -- Nod Position (Queue Position)
-local nodPosition = Vector(3395.75, 1261.25, 80.03) -- Replace with your desired position
-
+local nodPosition = Vector(646.266 ,-949.261,-143.719) -- Replace with your desired position
+local nodEnt      = Vector(-1025.52,-954.937,-143.719)
 -- NPC Arrival Interval
 local arrivalInterval = 16 -- in seconds
 
@@ -42,13 +48,14 @@ local npcPreviousPositions = {}
 -- Timer function to check availability and handle NPC arrivals
 timer.Create("QueueTimer", timerInterval, 0, function()
     print("QueueTimer triggered")
-    for _, npcInfo in ipairs(npcEntities) do
+    for idx, npcInfo in ipairs(npcEntities) do
         local npc = Entity(npcInfo.id)
         print("Checking NPC: " .. npcInfo.name)
 
         -- Check if NPC is valid
         if not IsValid(npc) then
             print("NPC is not valid: " .. npcInfo.name)
+            table.remove(npcEntities, idx)
         else
             print("NPC is valid: " .. npcInfo.name)
 
@@ -57,34 +64,35 @@ timer.Create("QueueTimer", timerInterval, 0, function()
             local previousPos = npcPreviousPositions[npcInfo.name] or Vector(0, 0, 0)
             npcPreviousPositions[npcInfo.name] = currentPos
 
-            local isMoving = currentPos:Distance(previousPos) > 1 -- Adjust the distance threshold as needed
-
+            local isMoving = npc:IsCurrentSchedule(SCHED_FORCED_GO)
+          
             if isMoving then
                 print("NPC is moving: " .. npcInfo.name)
-            end
-
-            -- Check if someone is in front of the NPC
-            if isSomeoneInFront(npc, nodPosition) then
-                print("Someone is in front of NPC: " .. npcInfo.name)
-            end
-
-            -- Rest of the script...
-
-            if not npcArrivalTimes[npcInfo.name] then
-                -- NPC hasn't arrived yet, check if the nod position is available
-                if not isMoving and not isSomeoneInFront(npc, nodPosition) then
-                    print("Moving NPC: " .. npcInfo.name)
-                    npc:SetPos(nodPosition)
-                    npcArrivalTimes[npcInfo.name] = CurTime() + arrivalInterval -- Set arrival time
-                end
             else
-                -- NPC has arrived, check if it's time to move to the next step
-                local currentTime = CurTime()
-                if currentTime >= npcArrivalTimes[npcInfo.name] then
-                    -- Move the NPC to the next destination (replace with your logic)
-                    print("Moving NPC to the next destination: " .. npcInfo.name)
-                    npc:SetPos(Vector(3100.36, 1596.41, 80.03))
-                    npcArrivalTimes[npcInfo.name] = nil -- Reset arrival time for the next cycle
+                -- Check if someone is in front of the NPC
+                if isSomeoneInFront(npc, nodPosition) then
+                    print("Someone is in front of NPC: " .. npcInfo.name)
+                end
+
+                -- Rest of the script...
+
+                if not npcArrivalTimes[npcInfo.name] then
+                    -- NPC hasn't arrived yet, check if the nod position is available
+                    if not isMoving and not isSomeoneInFront(npc, nodPosition) then
+                        print("Moving NPC: " .. npcInfo.name)
+                        npcMoveTo(npc, nodPosition)
+                        npcArrivalTimes[npcInfo.name] = CurTime() + arrivalInterval -- Set arrival time
+                    end
+                else
+                    -- NPC has arrived, check if it's time to move to the next step
+                    local currentTime = CurTime()
+                    if currentTime >= npcArrivalTimes[npcInfo.name] then
+                        -- Move the NPC to the next destination (replace with your logic)
+                        print("Moving NPC to the next destination: " .. npcInfo.name)
+                        
+                        npcMoveTo(npc, nodEnt)
+                        npcArrivalTimes[npcInfo.name] = nil -- Reset arrival time for the next cycle
+                    end
                 end
             end
         end
