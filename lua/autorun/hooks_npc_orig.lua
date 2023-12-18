4fac0bea991f761573df0bb6605021c3009ed730
dvdvideo1234 <dvd_video@abv.bg>
2023-12-18 14:58:17 +0200

    Updated queue + visuals TODO: Remove NPC when reaches out position

--- lua/autorun/hooks_npc_orig.lua
+++ lua/autorun/hooks_npc_orig.lua
@@ -2,8 +2,8 @@
 local mtDesk = {}
       -- Metatable method indexing
       mtDesk.__index = mtDesk
-      -- Where to go after 16 seconds
-      mtDesk.__out = Vector(646.266 ,-900,-143.719)
+      -- Where to go after pulling
+      mtDesk.__out = Vector(800, -400,-143.719)
       -- Table of all the NPC
       mtDesk.__npc = {}
       -- NPC Move type
@@ -29,14 +29,14 @@ local function newDesk(pos, dir, dst, siz)
   mvDir:Normalize()
   for idx = 1, miSiz do
     local muo = ((idx - 1) * mnDst)
-    self.Data[idx] = {Pos = Vector(), Ent = NULL}
-    self.Data[idx].Pos:Set(mvPos + muo * mvDir)
+    mtData[idx] = {Pos = Vector(), Ent = nil}
+    mtData[idx].Pos:Set(mvPos + muo * mvDir)
   end
   -- Check whenever NPC exists
-  function self:isHere(npc)
+  function self:IsHere(npc)
     if(not IsValid(npc)) then return end
-    for iqu = 1, miSiz do
-      local v = mtData[isr]
+    for idx = 1, miSiz do
+      local v = mtData[idx]
       if(IsValid(v.Ent) and v.Ent == npc) then
         return true
       end
@@ -49,96 +49,169 @@ local function newDesk(pos, dir, dst, siz)
     npc:SetSchedule(SCHED_FORCED_GO)
   end
   -- Check when full
-  function self:isFull()
+  function self:IsFull()
     return (mtData.Size >= miSiz)
   end
   -- Check when slot used
-  function self:npcUse(idx)
+  function self:IsIndex(idx)
     local idx = (tonumber(idx) or 0)
           idx = (idx > 0) and math.floor(idx) or 0
           idx = (idx > 0) and idx or 0
+    if(idx == 0) then return nil end
+    local dat = mtData[idx]
+    if(not dat) then return nil end
+    return (IsValid(dat.Ent) and idx or nil)
+  end
+  -- Check when slot used
+  function self:IsFront(idx)
+    local idx = (tonumber(idx) or 0)
+          idx = (idx > 0) and math.floor(idx) or 0
+          idx = (idx > 0) and idx or 0
+    if(idx == 0) then return nil end
     local pos = mtData[idx].Pos + Vector(0,0,15)
     local ent = ents.FindInSphere(pos, 5)
     return ent[1]
   end
   -- Push NPC at the end of the queue
-  function self:npcPush(npc)
-    if(self:isFull()) then return self end
+  function self:Push(npc)
+    if(self:IsFull()) then return self end
     if(not IsValid(npc)) then return self end
-    if(self:isHere(npc)) then return self end
+    if(self:IsHere(npc)) then return self end
     mtData.Size = mtData.Size + 1
     mtData[mtData.Size].Ent = npc
     return self
   end
   -- Pull NPC at the front of the queue
-  function self:npcPull()
+  function self:Pull()
+    if(mtData.Size == 0) then return nil end
     local npc = mtData[1].Ent
+    if(not IsValid(npc)) then return nil end
     mtData.Size = mtData.Size - 1
-    mtData[1].Ent = NULL
+    mtData[1].Ent = nil
+    print("PULL", npc)
     return npc
   end
   -- Rearange NPC in the queue
-  function self:npcWait()
+  function self:Arrange()
     local ius, ifr = 0, 1
     for iqu = 1, miSiz do
-      for isr = iqu, miSiz do
-        if(IsValid(mtData[isr].Ent)) then
-          ius = isr -- Save index of first valid
-        else
-          ius = 0 -- Could not find NPC
-          mtData[isr].Ent = NULL
+      if(IsValid(mtData[iqu].Ent)) then
+      else
+        for isr = (iqu + 1), miSiz do
+          if(IsValid(mtData[isr].Ent)) then
+            ius = isr -- Save index of first valid
+            print("FOUND", isr, mtData[isr].Ent)
+            break;
+          else
+            ius = 0 -- Could not find NPC
+            mtData[isr].Ent = nil
+          end
+        end -- When npc is found assign it to the empty slot
+        if(ius ~= 0 and not IsValid(mtData[iqu].Ent)) then
+          print("MOVE", ius, iqu, mtData[ius].Ent)
+          mtData[iqu].Ent = mtData[ius].Ent
         end
-      end -- When npc is found assign it to the empty slot
-      if(ius ~= 0 and not IsValid(mtData[isr].Ent)) then
-        mtData[isr].Ent = mtData[ius].Ent
       end
     end; return self
   end
-  function self:npcStay()
-    for iqu = 1, miSiz do
-      local v = mtData[isr]
+  function self:Stay()
+    for idx = 1, miSiz do
+      local v = mtData[idx]
       if(IsValid(v.Ent)) then
         self:npcMove(v.Ent, v.Pos)
       end
     end; return self
   end
+  function self:GerRadius(str, mar)
+    local pos = LocalPlayer():GetPos()
+    return (mar * 200) / str:Distance(pos)
+  end
+  function self:Draw()
+    cam.Start2D()
+      local str, idx = mtData[1].Pos, self:IsIndex(1)
+     -- print("S", 1, mtData[1].Ent)
+      local xy = str:ToScreen()
+      local cr, cg = (idx and 0 or 255), (idx and 255 or 0)
+      surface.SetDrawColor(cr, cg, 0)
+      surface.DrawCircle(xy.x, xy.y, self:GerRadius(str, 10), cr, cg, 0)
+      for cnt = 2, miSiz do
+       -- print("G", cnt, mtData[cnt].Ent)
+        local poc = mtData[cnt].Pos
+        local pop = mtData[cnt-1].Pos
+        local xyc = poc:ToScreen()
+        local xyp = pop:ToScreen()
+        idx = self:IsIndex(cnt)
+        cr = (idx and 0 or 255)
+        cg = (idx and 255 or 0)
+        surface.SetDrawColor(cr, cg, 0)
+        surface.DrawLine(xyp.x, xyp.y, xyc.x, xyc.y)
+        surface.DrawCircle(xyc.x, xyc.y, self:GerRadius(poc, 10), cr, cg, 0)
+      end
+      local poo = mtDesk.__out
+      local xyo = poo:ToScreen()
+      surface.SetDrawColor(255, 255, 0)
+      surface.DrawLine(xy.x, xy.y, xyo.x, xyo.y)
+      surface.DrawCircle(xyo.x, xyo.y, self:GerRadius(poo, 20), 255, 255, 0)
+    cam.End2D()
+    return self
+  end
   return self
 end
 
-local oDesk = newDesk(Vector(646.266 ,-949.261,-143.719), Vector(1,0,0), 16, 20)
+local oDesk = newDesk(Vector(646.266 ,-949.261,-143.719), Vector(-1,0,0), 100, 10)
 
+if(not oDesk) then error("Could not allocate desk object!") end
+
+hook.Remove("PlayerSpawnedNPC", "hook_npc_queue")
 hook.Add("PlayerSpawnedNPC", "hook_npc_queue",
   function(ply, npc)
     if(not IsValid(npc)) then return end
     mtDesk.__npc[tostring(npc:EntIndex())] = npc
-    oDesk:npcPush(npc)
+    oDesk:Push(npc)
+  end)
+
+hook.Remove("PreDrawHUD", "hook_npc_queue_cl")
+hook.Add("PreDrawHUD", "hook_npc_queue_cl",
+  function()
+    oDesk:Draw()
   end)
 
 -- Timer function to check availability and handle NPC arrivals
 -- It will constantly try to pit NPCs at the queue desk
-timer.Create("QueueTimerPush", mtDesk.__push, 0, function()
+timer.Remove("hook_npc_queue_push")
+timer.Create("hook_npc_queue_push", mtDesk.__push, 0, function()
   for idx, npc in pairs(mtDesk.__npc) do
-    oDesk:npcPush(npc):npcWait():npcStay()
+    oDesk:Push(npc):Arrange():Stay()
   end
 end)
 
 -- Timer function to check when NPC leaves
 local puNPC
-timer.Create("QueueTimerPull", mtDesk.__pull, 0, function()
+timer.Remove("hook_npc_queue_pull")
+timer.Create("hook_npc_queue_pull", mtDesk.__pull, 0, function()
   if(IsValid(puNPC)) then
+    print("PUVV", mtDesk.__dstr, puNPC)
     if(not puNPC:IsCurrentSchedule(mtDesk.__move)) then
+      print("PURT", mtDesk.__dstr, puNPC)
       timer.Simple(mtDesk.__dstr,
         function()
+          print("PURM", puNPC)
           SafeRemoveEntity(puNPC)
-          puNPC = NULL
+          puNPC = nil
         end)
     end
   else
-    local puNPC = oDesk:npcPull()
-    if(IsValid(npc)) then
-      oDesk:npcMove(npc, mtDesk.__out)
+    local puNPC = oDesk:Pull()
+    print("PUNW", puNPC)
+    if(IsValid(puNPC)) then
+      print("PUMV", puNPC, mtDesk.__out)
+      oDesk:npcMove(puNPC, mtDesk.__out)
+      mtDesk.__npc[tostring(puNPC:EntIndex())] = nil
+      oDesk:Arrange():Stay()
     else
-      puNPC = NULL
+      print("PUCL", puNPC)
+      puNPC = nil
     end
   end
 end)
+
