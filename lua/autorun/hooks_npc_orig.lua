446e7f6a2c4fc227dcfe136d3ed495be1d02666d
dvdvideo1234 <dvd_video@abv.bg>
2023-12-20 11:52:23 +0200

    Added: Hove mind or single mind configuration

--- lua/autorun/hooks_npc_orig.lua
+++ lua/autorun/hooks_npc_orig.lua
@@ -1,37 +1,43 @@
       -- Metatable of the desc class. Global data
-local mtDesk = {}
+local mtQueue = {}
       -- Metatable method indexing
-      mtDesk.__index = mtDesk
+      mtQueue.__index = mtQueue
       -- Where to go after pulling
-      mtDesk.__out = Vector(2635.5,1241.34,16.2813)
+      mtQueue.__out = Vector(2635.5,1241.34,16.2813)
+      -- Where to go after pulling
+      mtQueue.__cout = Color(255,255,0,255)
       -- Table of all the NPC
-      mtDesk.__npc = {}
+      mtQueue.__npc = {}
       -- Caontains the NPC which exits
-      mtDesk.__npx = nil
+      mtQueue.__npx = nil
+      -- Enable to act as a hive mind
+      mtQueue.__hive = true
       -- NPC Move type
-      mtDesk.__move = SCHED_FORCED_GO
+      mtQueue.__move = SCHED_FORCED_GO_RUN
       -- NPC Exit Interval
-      mtDesk.__pull = 16 -- in seconds
+      mtQueue.__pull = 16 -- in seconds
       -- NPC Arrival Interval
-      mtDesk.__push = 2 -- in seconds
+      mtQueue.__push = 2 -- in seconds
       -- Check when shedule is finished
-      mtDesk.__shed = 0.1 -- in seconds
+      mtQueue.__shed = 0.1 -- in seconds
       -- Remove after the final destination
-      mtDesk.__dstr = 1 -- in seconds
+      mtQueue.__dstr = 1 -- in seconds
       -- Amount of units to scan for NPC
-      mtDesk.__tnpc = 100
+      mtQueue.__tnpc = 100
+      -- Color to pass for drawing
+      mtQueue.__colr = Color(0,0,0,255)
       -- Function filter for NPC trace
-      mtDesk.__trft = {
+      mtQueue.__trft = {
         start  = Vector(), -- Start position
         endpos = Vector(), -- End position
         ignoreworld = true, -- Ignore hitting world
         filter = function(ent) return ent:IsNPC() end
       }
-local function newDesk(pos)
+local function NewQueue(pos)
   local mvPos, miSiz = Vector(pos), 1
   local mtData = {Size = 0}
         mtData[1] = {Pos = Vector(mvPos), Ent = nil}
-  local self = {}; setmetatable(self, mtDesk)
+  local self = {}; setmetatable(self, mtQueue)
   -- Extend the desk queue
   function self:Extend(dir, dst, siz)
     local vDir = Vector(dir); vDir:Normalize()
@@ -90,7 +96,7 @@ local function newDesk(pos)
   end
   -- Check whenever NPC exists
   function self:IsHere(npc)
-    if(not IsValid(npc)) then return end
+    if(not IsValid(npc)) then return false end
     for idx = 1, miSiz do
       local v = mtData[idx]
       if(IsValid(v.Ent) and v.Ent == npc) then
@@ -100,9 +106,17 @@ local function newDesk(pos)
   end
   -- Move NPC to desired position
   function self:Move(npc, pos)
-    if(not IsValid(npc)) then return end
+    if(not IsValid(npc)) then return self end
     npc:SetLastPosition(pos)
-    npc:SetSchedule(mtDesk.__move)
+    npc:SetSchedule(mtQueue.__move)
+    return self
+  end
+  -- Is the NPC still moving
+  function self:IsMove(npc, pos)
+    if(not IsValid(npc)) then return false end
+    if(npc:IsCurrentSchedule(mtQueue.__move)) then return true end
+    if(pos and npc:GetPos():DistToSqr(pos) > 10) then return true end
+    return false -- NPC has finished moving
   end
   -- Check when full
   function self:IsFull()
@@ -112,14 +126,13 @@ local function newDesk(pos)
   function self:GetTrace(idx)
     local idx = self:IsIndex(idx)
     if(not idx) then return nil end
-    local mar = Vector(0,0,mtDesk.__tnpc)
+    local mar = Vector(0,0,mtQueue.__tnpc)
     local pos = mtData[idx].Pos
-    local dat = mtDesk.__trft
+    local dat = mtQueue.__trft
           dat.start:Set(pos); dat.start:Add(mar)
           mar.z = -mar.z -- We need for top to bottom
           dat.endpos:Set(pos); dat.endpos:Add(mar)
-    local trn = util.TraceLine(mtDesk.__trft)
-    return trn.Hit -- NPC has been hit
+    return util.TraceLine(mtQueue.__trft)
   end
   -- Push NPC at the end of the queue
   function self:Push(npc)
@@ -143,88 +156,113 @@ local function newDesk(pos)
   function self:Arrange()
     local idx, siz = 0, 0
     for crr = 1, miSiz do
-      if(IsValid(mtData[crr].Ent)) then
+      local cv = mtData[crr]
+      if(IsValid(cv.Ent)) then
         siz = siz + 1 -- Regster populated node
       else -- Fill it from the next set
         idx = 0 -- Assume valid NPC is not found
         for src = (crr + 1), miSiz do
-          if(IsValid(mtData[src].Ent)) then
+          local sv = mtData[src]
+          if(IsValid(sv.Ent)) then
             idx = src; break
           else -- Save index of first valid
-            mtData[src].Ent = nil
+            sv.Ent = nil
           end
         end -- When npc is found assign it to the empty slot
-        if(idx ~= 0 and not IsValid(mtData[crr].Ent)) then
-          mtData[crr].Ent = mtData[idx].Ent
-          mtData[idx].Ent = nil; siz = siz + 1
+        if(idx ~= 0 and not IsValid(cv.Ent)) then
+          local iv = mtData[idx]
+          cv.Ent = iv.Ent -- Move NPC to current pointer
+          iv.Ent = nil    -- Remove the NPC from the slot
+          siz = siz + 1   -- Increment NPC count
         end
       end
     end -- Assign the new NPC count
-    if(siz > 0) then mtData.Size = siz end
-    return self
+    if(siz > 0) then mtData.Size = siz end; return self
   end
-  function self:Stay()
+  function self:Relocate()
     for idx = 1, miSiz do
       local v = mtData[idx]
       if(IsValid(v.Ent)) then
-        self:Move(v.Ent, v.Pos)
+        if(mtQueue.__hive) then
+          self:Move(v.Ent, v.Pos)
+        else
+          local mv = self:IsMove(v.Ent)
+          local tr = self:GetTrace(idx)
+          if((tr and not tr.Hit) and not mv) then
+            self:Move(v.Ent, v.Pos)
+          end
+        end
       end
     end; return self
   end
-  function self:GerRadius(org, mar)
+  -- Apply perspective radius
+  function self:GetRadius(org, mar)
     local pos = LocalPlayer():GetPos()
     return (mar * 200) / org:Distance(pos)
   end
+  -- Calculate colors
+  function self:GetColor(idx)
+    local co = mtQueue.__colr
+    local idx = self:IsIndex(idx)
+    if(not idx) then return co end
+    local hit = self:GetTrace(idx).Hit
+    co.r = (hit and 0 or 255)
+    co.g = (hit and 255 or 0)
+    return co
+  end
+  --Draw debig information
   function self:Draw()
-    local str, hit = mtData[1].Pos, self:GetTrace(1)
+    local str = mtData[1].Pos
+    local cor = self:GetColor(1)
     local xy = str:ToScreen()
-    local cr, cg = (hit and 0 or 255), (hit and 255 or 0)
-    surface.SetDrawColor(cr, cg, 0)
-    surface.DrawCircle(xy.x, xy.y, self:GerRadius(str, 10), cr, cg, 0)
+    surface.SetDrawColor(cor)
+    surface.DrawCircle(xy.x, xy.y, self:GetRadius(str, 10), cor)
     for cnt = 2, miSiz do
       local poc = mtData[cnt].Pos
       local pop = mtData[cnt-1].Pos
       local xyc = poc:ToScreen()
       local xyp = pop:ToScreen()
-      hit = self:GetTrace(cnt)
-      cr = (hit and 0 or 255)
-      cg = (hit and 255 or 0)
-      surface.SetDrawColor(cr, cg, 0)
+      local ccr = self:GetColor(cnt)
+      surface.SetDrawColor(ccr)
       surface.DrawLine(xyp.x, xyp.y, xyc.x, xyc.y)
-      surface.DrawCircle(xyc.x, xyc.y, self:GerRadius(poc, 10), cr, cg, 0)
+      surface.DrawCircle(xyc.x, xyc.y, self:GetRadius(poc, 10), ccr)
     end
-    local poo = mtDesk.__out
+    local poo = mtQueue.__out
     local xyo = poo:ToScreen()
-    surface.SetDrawColor(255, 255, 0)
+    surface.SetDrawColor(mtQueue.__cout)
     surface.DrawLine(xy.x, xy.y, xyo.x, xyo.y)
-    surface.DrawCircle(xyo.x, xyo.y, self:GerRadius(poo, 20), 255, 255, 0)
+    surface.DrawCircle(xyo.x, xyo.y, self:GetRadius(poo, 20), mtQueue.__cout)
     return self
   end
   return self
 end
 
-local oDesk = newDesk(Vector(3369.71,1296.38,16.2813))
-      oDesk:Extend(Vector(1,0,0), 60, 1)
-      oDesk:SetNode(2, Vector(3400.85,1357.97,16.2813))
-      oDesk:Extend(Vector(1,0,0), 60, 1)
-      oDesk:SetNode(3, Vector(3397.96,1433.85,16.2813))
-      oDesk:Extend(Vector(1,0,0), 60, 1)
-      oDesk:SetNode(4, Vector(3374.88,1486.17,16.2812))
-      oDesk:Extend(Vector(-1,0,0), 60, 6)
-      oDesk:Extend(Vector(0,-1,0), 60, 2)
-      oDesk:Extend(Vector(-1,0,0), 60, 1)
-      oDesk:Extend(Vector(0,1,0), 60, 2)
-      oDesk:Extend(Vector(-1,0,0), 60, 1)
-      oDesk:Extend(Vector(0,-1,0), 60, 2)
+local oQ = NewQueue(Vector(3362.8,1268.72,16.2813))
+      oQ:Extend(Vector(1,0,0), 60, 1)
+      oQ:SetNode(2, Vector(3397.98,1317.09,16.2813))
+      oQ:Extend(Vector(1,0,0), 60, 1)
+      oQ:SetNode(3, Vector(3403.37,1374.76,16.2813))
+      oQ:Extend(Vector(1,0,0), 60, 1)
+      oQ:SetNode(4, Vector(3397.85,1433.04,16.2813))
+      oQ:Extend(Vector(1,0,0), 60, 1)
+      oQ:SetNode(5, Vector(3374.88,1486.17,16.2812))
+      oQ:Extend(Vector(-1,0,0), 60, 6)
+      oQ:Extend(Vector(0,-1,0), 60, 2)
+      oQ:Extend(Vector(-1,0,0), 60, 1)
+      oQ:Extend(Vector(0,1,0), 60, 2)
+      oQ:Extend(Vector(-1,0,0), 60, 1)
+      oQ:Extend(Vector(0,-1,0), 60, 2)
+      oQ:Extend(Vector(-1,0,0), 60, 1)
+      oQ:Extend(Vector(0,1,0), 60, 2)
 
-if(not oDesk) then error("Failed allocating desk object!") end
+if(not oQ) then error("Failed allocating desk object!") end
 
 if(CLIENT) then
   hook.Remove("PreDrawHUD", "hook_npc_queue_cl")
   hook.Add("PreDrawHUD", "hook_npc_queue_cl",
     function()
       cam.Start2D()
-      oDesk:Draw()
+      oQ:Draw()
       cam.End2D()
     end)
 else
@@ -232,45 +270,44 @@ else
   hook.Add("PlayerSpawnedNPC", "hook_npc_queue",
     function(ply, npc)
       if(not IsValid(npc)) then return end
-      mtDesk.__npc[tostring(npc:EntIndex())] = npc
-      oDesk:Push(npc)
+      mtQueue.__npc[tostring(npc:EntIndex())] = npc
+      oQ:Push(npc)
     end)
 
   -- Timer function to check availability and handle NPC arrivals
   -- It will constantly try to pit NPCs at the queue desk
   timer.Remove("hook_npc_queue_push")
-  timer.Create("hook_npc_queue_push", mtDesk.__push, 0,
+  timer.Create("hook_npc_queue_push", mtQueue.__push, 0,
     function()
-      for idx, npc in pairs(mtDesk.__npc) do
-        oDesk:Push(npc):Arrange():Stay()
+      for idx, npc in pairs(mtQueue.__npc) do
+        oQ:Push(npc):Arrange():Relocate()
       end
     end)
 
   -- Timer function to check when NPC leaves
   timer.Remove("hook_npc_queue_pull")
-  timer.Create("hook_npc_queue_pull", mtDesk.__pull, 0,
+  timer.Create("hook_npc_queue_pull", mtQueue.__pull, 0,
     function()
-      if(IsValid(mtDesk.__npx)) then return end
-      local ent = oDesk:GetEntity(1)
-      if(IsValid(ent) and ent:IsCurrentSchedule(mtDesk.__move)) then return end
-      mtDesk.__npx = oDesk:Pull()
-      if(IsValid(mtDesk.__npx)) then
-        oDesk:Move(mtDesk.__npx, mtDesk.__out)
-        mtDesk.__npc[tostring(mtDesk.__npx:EntIndex())] = nil
+      if(IsValid(mtQueue.__npx)) then return end
+      local ent = oQ:GetEntity(1)
+      if(IsValid(ent) and oQ:IsMove(ent)) then return end
+      mtQueue.__npx = oQ:Pull()
+      if(IsValid(mtQueue.__npx)) then
+        oQ:Move(mtQueue.__npx, mtQueue.__out)
+        mtQueue.__npc[tostring(mtQueue.__npx:EntIndex())] = nil
       end
-      oDesk:Arrange():Stay()
+      oQ:Arrange():Relocate()
     end)
 
   timer.Remove("hook_npc_queue_ched")
-  timer.Create("hook_npc_queue_ched", mtDesk.__shed, 0,
+  timer.Create("hook_npc_queue_ched", mtQueue.__shed, 0,
     function()
-      if(not IsValid(mtDesk.__npx)) then return end
-      if(mtDesk.__npx:IsCurrentSchedule(mtDesk.__move)) then return end
-      if(mtDesk.__npx:GetPos():DistToSqr(mtDesk.__out) > 10) then return end
-      timer.Simple(mtDesk.__dstr,
+      if(not IsValid(mtQueue.__npx)) then return end
+      if(oQ:IsMove(mtQueue.__npx, mtQueue.__out)) then return end
+      timer.Simple(mtQueue.__dstr,
         function()
-          SafeRemoveEntity(mtDesk.__npx)
-          mtDesk.__npx = nil
+          SafeRemoveEntity(mtQueue.__npx)
+          mtQueue.__npx = nil
         end)
     end)
 end
